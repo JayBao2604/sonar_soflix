07f6efc72483572148183d052546d669fbb283e3
TehDoggo420 <vudeptrai0099@gmail.com>
2024-05-18 17:24:18 +0700

    fix search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,44 +6,8 @@ import Logo from "../../public/sonar.png";
 import Link from "next/link";
 import { Home } from "lucide-react";
 import prisma from "../utils/db";
+import { getData } from "./getSearchData";
 
-export const getData = async (query: string, userId: string) => {
-    try {
-      const fieldsToSearch = ['singer', 'title', 'album', 'category']; 
-      const conditions = fieldsToSearch.map(field => ({
-        [field]: {
-          contains: query,
-          mode: "insensitive",
-        },
-      }));
-  
-      const data = await prisma.song.findMany({
-        where: {
-          OR: conditions,
-        },
-        select: {
-            id: true,
-            singer: true,
-            title: true,
-            SongLists: {
-              where: {
-                userId: userId,
-              },
-            },
-            imageString: true,
-            youtubeString: true,
-            release: true,
-            duration: true,
-            album: true,
-            category: true,
-            artist: true,
-        }, 
-      });
-      return data;
-    } catch (error) {
-      throw new Error("Failed to fetch data");
-    }
-  };
 
 
 const SearchPage = async ({
